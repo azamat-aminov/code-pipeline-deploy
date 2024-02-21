6e6bb56f98fa3fb034a795e6df1696d6bf05f35f
azamat-aminov <azamatmaqsudjonovich@gmail.com>
2024-02-21 11:52:34 +0500

    server start .sh file modified

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar code-pipeline-deploy-0.0.1-SNAPSHOT.jar
+sudo java -jar code-pipeline-deploy-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
